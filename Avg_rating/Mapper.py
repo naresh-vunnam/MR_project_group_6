25eec979af59c0d9527567c56c519a353bb92d20
Hitesh Kolla <S530744>
2018-11-19 21:28:54 -0600

    added mapper and reducer for averge ratings

--- Avg_rating/Mapper.py
+++ Avg_rating/Mapper.py
@@ -1,9 +1,13 @@
-f = open("d.txt","r")  # open file, read-only raw data
-o = open("o.txt", "w") # open file, write - just our key, value pairs
-for line in f:  
-    data = line.strip().split("    ") 
-    if len(data) == 6:
-        date, time, store, item, cost, payment = data
-        o.write("{0}\t{1}\n".format(store, cost))
+f = open("googleplaystore.txt", "r")  # open file, read-only raw data
+o = open("mappedGooglePlayStore.txt", "w") # open file, write - just our key, value pairs
+
+for line in f:                     #reads in file line by line
+    
+    data = line.strip().split("\t")
+    
+    if len(data) == 13:      #confirms each line is the correct size
+        App, Category, Rating, Reviews, Size, Installs, Type, Price, Content_Rating, Genres, Last_Updated, Current_Ver, Android_Ver = data   #takes in each value into data
+        o.write("{0}\t{1}\n".format(Category, Rating))    #writes to file category and review
+        
 f.close()
 o.close()
--- Avg_rating/Reducer.py
+++ Avg_rating/Reducer.py
@@ -1,19 +1,40 @@
-or line in s:
- data = line.strip().split('\t')
- store, amount = data
+s = open("sortedGooglePlayStore.txt", "r")   #opens file, read only
+r = open("reducedGooglePlayStore.txt", "w")  #creates or reopens export file
 
- if store != thisKey:
-   if thisKey:
-     # output the last key value pair result
-     r.write(thisKey + '\t' + str(thisValue)+'\n')
+oldkey = None
+thisKey = ""
+thisValue = 0.0
+count = 0.0
 
-   # start over when changing keys
-   thisKey = store
-   thisValue = 0.0
- # apply the aggregation function
- thisValue += float(amount)
-# output the final entry when done (outside for loop)
-r.write(thisKey + '\t' + str(thisValue)+'\n')
+for line in s:
+    data = line.strip().split('\t')
+    if len(data) != 2:  # if bad input line
+        continue  # ignore it
+
+# read into variables
+    Category, Rating = data  
+
+    if Category != thisKey:
+        if thisKey:
+# resultant ouput in the form of key value pair
+            r.write(thisKey + '\t' + str(thisValue/count) + '\n')
+            print(thisKey + '\t' + str(thisValue/count) + '\n')
+
+# start over when changing keys
+        thisKey = Category
+        thisValue = 0.0
+        count = 0.0
+
+# apply the aggregation function
+    thisValue += float(Rating)
+# apply the count function
+    count += 1
+# final ouput in the key value pair i.e.,(country, average)
+r.write(thisKey + '\t' + str(thisValue/count) + '\n')
+print(thisKey + '\t' + str(thisValue/count) + '\n')
 
 s.close()
 r.close()
+
+
+

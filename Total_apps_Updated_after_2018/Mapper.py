629fb9b87c4c70f6833306232422b67de0c4d169
nvunnam <S530492@nwmissouri.edu>
2018-11-27 10:42:36 -0600

    update working mapper and reducer and outputs

--- Total_apps_Updated_after_2018/Mapper.py
+++ Total_apps_Updated_after_2018/Mapper.py
@@ -1,9 +1,16 @@
-f = open("d.txt","r")  # open file, read-only raw data
-o = open("o.txt", "w") # open file, write - just our key, value pairs
-for line in f:  
-    data = line.strip().split("    ") 
-    if len(data) == 6:
-        date, time, store, item, cost, payment = data
-        o.write("{0}\t{1}\n".format(store, cost))
+f = open("googleplaystore.txt","r")  # open file, read-only raw data
+o = open("mappedGooglePlayStore.txt", "w") # open file, write - just our key, value pairs
+
+for line in f:                     #reads in file line by line
+    
+    data = line.strip().split("\t")
+    
+    if len(data) == 13:      #confirms each line is the correct size
+     #takes in each value into data
+     category = data[1]
+     lastUpdate = data[10]
+     o.write("{0}\t{1}\n".format(category, lastUpdate))    #writes to file category and review
+        
 f.close()
 o.close()
+
